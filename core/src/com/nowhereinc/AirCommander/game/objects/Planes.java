0b4bcfdc541247db5b092fc89677b4bd92e66815
Brian Will <mnpuck@hotmail.com>
2015-05-07 21:42:08 -0500

    Working on collision logic

--- core/src/com/nowhereinc/AirCommander/game/objects/Planes.java
+++ core/src/com/nowhereinc/AirCommander/game/objects/Planes.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
-import com.nowhereinc.AirCommander.game.Assets;
 import com.nowhereinc.AirCommander.game.WorldRenderer;
 import com.nowhereinc.AirCommander.util.Constants;
 
@@ -28,14 +27,15 @@ public class Planes {
 	private float boxXSize;
 	private float boxYSize;
 	
-	public Planes (World world) {
-		init(world);
+	public Planes (World world, String planeNumber) {
+		init(world, planeNumber);
 	}
 	
-	private void init(World world) {
+	private void init(World world, String planeNumber) {
 		
 		// create body def for player
 		bodyDefPlane = new BodyDef();
+		bodyDefPlane.position.set(0f, 0f);
 		bodyDefPlane.type = BodyDef.BodyType.DynamicBody;
 		body = world.createBody(bodyDefPlane);
 		
@@ -49,9 +49,58 @@ public class Planes {
 		fixtureDefPlane = new FixtureDef();
 		fixtureDefPlane.shape = shape;
 		fixtureDefPlane.restitution = 0;
+		body.createFixture(fixtureDefPlane).setUserData(planeNumber);
 		
 		shape.dispose();
 		
 	}
+	
+	public Vector2 returnPlanePosition() {
+		
+		return this.body.getPosition();
+			
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void setDeleteFlag() {
+		
+		body.setUserData("delete");
 
+	}
+	
+	public void deletePlane(World world) {
+		
+		if(body.getUserData() == "delete") {
+		
+			// delete body
+		
+			body.setActive(false);
+			body.setUserData(null);
+			body = null;
+		
+		}
+		
+	}
+
+	public void render (SpriteBatch batch) {
+			
+		if (body.getUserData() != "delete") {
+					
+			Vector2 position = this.body.getPosition();
+				
+			position.x = position.x - boxXSize;
+			position.y = position.y - boxYSize;
+
+			float rotation = (MathUtils.radiansToDegrees * this.body.getAngle());
+			
+			Plane = (TextureRegion)this.body.getUserData();
+			batch.draw(Plane, position.x, position.y, 0, 0, boxXSize * 2, boxYSize * 2, 1, 1, rotation);
+				
+		}		
+		
+	}
+	
 }
